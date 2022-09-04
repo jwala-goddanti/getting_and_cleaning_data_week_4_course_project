e3e9e5b74deb5e40623765b7b6a629e493036897
jwala-goddanti <111846733+jwala-goddanti@users.noreply.github.com>
2022-09-04 11:00:40 +0530

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,13 @@
+library(plyr)
+ 
+if(!file.exists("./getcleandata")){dir.create("./getcleandata")}
+fileurl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+
+        mean_and_std <- (grepl("activityID", colNames) |
+                         grepl("subjectID", colNames) |
+                         grepl("mean..", colNames) |
+                         grepl("std...", colNames)
+        )
+        write.table(tidySet, "tidySet.txt", row.names = FALSE)
+
+
